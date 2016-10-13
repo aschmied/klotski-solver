4ead48b16e181f70c50290d421b44fb98abeb84d
Anthony Schmieder <anthony@primatelabs.com>
2016-10-12 23:00:47 -0400

    skeleton main and Solver

---
+++ main.py
@@ -0,0 +1,8 @@
+import p1.solver
+
+print "Beginning solution..."
+
+solver = p1.solver.Solver()
+solution, stats = solver.solve()
+
+print "Solution complete"
---
+++ p1/solver.py
@@ -0,0 +1,6 @@
+class Solver:
+  def __init__(self):
+    pass
+
+  def solve(self):
+    return None, None
