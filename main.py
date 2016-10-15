d61c5f0a6c97b3ec4d0ab222dc48a256cedf5b5d
Anthony Schmieder <anthony@primatelabs.com>
2016-10-14 22:55:43 -0400

    Add timing and print the solution

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import time
+
 import p1.solver
 
-print "Beginning solution..."
-
+t0 = time.time()
 solver = p1.solver.Solver()
 solution = solver.solve()
+t1 = time.time()
 
-print "Solution complete"
+print "Found solution in {} seconds".format(t1 - t0)
+print "Solution has {} moves".format(len(solution) - 1)
+
+for board in solution:
+  print(board)
+  print
